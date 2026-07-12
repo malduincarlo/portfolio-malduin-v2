51420fb6a4fc987f2afd0fab80e24245477acc34
Alduin <malduincarlo@gmail.com>
2026-07-12 21:34:46 +0800

    added redirection to resume

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -3,5 +3,8 @@ export const navigationItems = [
   { label: "about", href: "#about" },
   { label: "works and experience", href: "#works" },
   { label: "gallery", href: "#gallery" },
-  { label: "resume", href: "#resume" },
+  {
+    label: "resume",
+    href: "https://drive.google.com/file/d/1yBNfzWfq5gpYOEkKtWz-rM2D_sMUyj8K/view?usp=sharing",
+  },
 ];
